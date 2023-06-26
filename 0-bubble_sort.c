49e25ab3c3ad937d2a923201fa94ee4a54a64224
sayedhany <sayedh@mail.ru>
2023-06-26 15:32:36 -0700

    solveD

---
+++ 0-bubble_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * swap - function to swap two numbers
+ * @n1: num 1
+ * @n2: num 2
+ */
+void swap(int *n1, int *n2)
+{
+	int temp = *n1;
+	*n1 = *n2;
+	*n2 = temp;
+}
+
+/**
+ * bubble_sort - algorithm to sort data
+ * @array: array to be sorted
+ * @size: length of array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	bool swaped;
+
+	for (i = 0; i < size; i++)
+	{
+		swaped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				swaped = true;
+			}
+		}
+		if (swaped == false)
+			break;
+	}
+}
--- sort.h
+++ sort.h
@@ -1,5 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
+#include <stdio.h>
+#include <stdbool.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,4 +17,9 @@ typedef struct listint_s
 } listint_t;
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
 #endif

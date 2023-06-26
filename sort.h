520eac3d5363abeab808acba71633eee52ec6e1b
sayedhany <sayedh@mail.ru>
2023-06-26 15:37:16 -0700

    solveD

--- sort.h
+++ sort.h
@@ -15,6 +15,7 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+void swap(int *n1, int *n2);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
